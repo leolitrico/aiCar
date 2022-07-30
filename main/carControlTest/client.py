fd5813bdcdc2e3b137fc1c751b14962c0e70a4aa
leolitrico <leolitrico@gmail.com>
2022-07-30 15:46:05 +0200

    added exception handling

--- main/carControlTest/client.py
+++ main/carControlTest/client.py
@@ -25,6 +25,12 @@ while True:
     direction = data[0]
     forward = data[1]
     throttle = data[2]
+    intThrottle = 0
+
+    try:
+        intThrottle = int(throttle)
+    except:
+        print("bad communication format from network")
 
     
     if direction == "right":
@@ -35,10 +41,10 @@ while True:
         steering.steer(steering.Steering.STRAIGHT)
 
     if forward == "forward":
-        motor.run(-1 * int(throttle))
+        motor.run(-1 * intThrottle)
     elif forward == "none":
         motor.run(0)
     else:
-        motor.run(int(throttle))
+        motor.run(intThrottle)
     
     print(direction + " " + forward + ":" + throttle) 
