4f762b811af8cb2c56dfe517ed76f24a22c12db5
leolitrico <leolitrico@gmail.com>
2022-08-03 00:05:57 +0200

    fkljl

--- main/main.py
+++ main/main.py
@@ -11,15 +11,20 @@ import carSetup
 import imageClassification
 import server
 
+serverOn = False
+
 #setup, or stop locomotion if keyboard interrupt received
 try:
     threadQueue = Queue() #initialise thread queue
     carSetup.setup() #setup car control
-    sock = server.setupServer() #setup server
+
+    sock = None
+    if serverOn:
+        sock = server.setupServer() #setup server
 
     #initialise carControl thread, and object detection thread and start them 
     t1 = Thread(target=carThread.consumer, args=(threadQueue, ))
-    t2 = Thread(target=objectDetection.producer, args=(threadQueue, imageClassification.getInterpreter(), None, ))
+    t2 = Thread(target=objectDetection.producer, args=(threadQueue, imageClassification.getInterpreter(), sock, ))
     t1.start()
     t2.start()
 except KeyboardInterrupt:
